c2ef92277129c9ecc914c2ae6483a4047319eb04
Amr <frank1276smith@gmail.com>
2023-12-19 01:45:35 +0200

    database connection

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,6 +1,6 @@
 import { INewUser } from "@/types";
 import { ID } from "appwrite";
-import { account } from "./config";
+import { account, appwriteConfig, avatars, database } from "./config";
 
 export async function createUserAccount(user : INewUser){
     try {
@@ -9,11 +9,41 @@ export async function createUserAccount(user : INewUser){
             user.email,
             user.password,
             user.name
-        )
-        return newAccount;
-        
+        );
+        if(!newAccount) throw Error;
+
+        const avatarUrl = avatars.getInitials(user.name);
+        const newUser = await saveUserToDB({
+            accountId: newAccount.$id,
+            name: newAccount.name,
+            email: newAccount.email,
+            username: user.username,
+            imageUrl: avatarUrl,
+        })
+        return newUser;
+
     } catch (errorMsg) {
         console.log(errorMsg);
         return errorMsg;
+    }
+}
+
+export async function saveUserToDB(user: {
+    accountId: string,
+    email: string,
+    name: string,
+    imageUrl: URL,
+    username?: string
+}) {
+    try {
+        const newUser = await database.createDocument(
+            appwriteConfig.databaseId,
+            appwriteConfig.userCollectionId,
+            ID.unique(),
+            user,
+        )
+        return newUser;
+    } catch (errorMsg) {
+        console.log(errorMsg);
     }
 }
