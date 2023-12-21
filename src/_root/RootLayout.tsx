887a0154758c0b1a7d70975bf2399b78eb7ec013
Amr <frank1276smith@gmail.com>
2023-12-21 14:38:08 +0200

    import the new shared ui components ui

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,8 +1,19 @@
+import Bottombar from "@/components/ui/shared/Bottombar"
+import LeftSidebar from "@/components/ui/shared/LeftSidebar"
+import Topbar from "@/components/ui/shared/Topbar"
+import { Outlet } from "react-router-dom"
 
 const RootLayout = () => {
   return (
-    <div>
-      <h1>Welcome to home page</h1>
+    <div className="w-full md:flex">
+      <Topbar />
+      <LeftSidebar />
+
+      <section className="flex flex-1 h-full">
+        <Outlet />
+      </section>
+
+      <Bottombar />
     </div>
   )
 }
