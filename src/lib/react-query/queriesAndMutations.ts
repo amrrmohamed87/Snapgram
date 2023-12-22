8ff8f93a7fe98bf5e5246f84a0584dd14a5f740f
Amr <frank1276smith@gmail.com>
2023-12-22 09:34:03 +0200

    adding useSignOutAccount  function

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -2,19 +2,26 @@ import {
 useQuery, //for fetching data
 useMutation, //for modifying the data
 useQueryClient,
-useInfiniteQuery
+useInfiniteQuery,
+useMutationState
 } from '@tanstack/react-query'
-import { createUserAccount, signInAccount } from '../appwrite/api'
+import { createUserAccount, signInAccount, signOutAccount } from '../appwrite/api'
 import { INewUser } from '@/types'
 
 export const useCreateUserAccount = () => {
     return useMutation({
         mutationFn: (user: INewUser) => createUserAccount(user)
     })
-}
+};
 
 export const useSignInAccount = () => {
     return useMutation({
         mutationFn: (user: { email: string, password: string}) => signInAccount(user)
     })
-}
+};
+
+export const useSignOutAccount = () => {
+    return useMutation({
+        mutationFn: signOutAccount,
+    });
+};
